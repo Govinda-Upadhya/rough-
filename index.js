90c1437c23735e03e7860597ffbd727eecf2cfa7
Govinda <143345467+Govinda-Upadhya@users.noreply.github.com>
2025-08-01 15:29:06 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import e from "express";
 
 const app = e();
 
-app.get("/", async (res, req) => {
+app.get("/", async (req, res) => {
   return res.send("hello world this is the home page");
 });
-app.get("/todos", async (res, req) => {
+app.get("/todos", async (req, res) => {
   return res.send("this is a the todos list");
 });
 app.listen(3000, () => console.log("listening on port 3000"));
